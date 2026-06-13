297c634bd56c30a08e8e8bdbc6f961e868982d74
Abhidixit-11 <abhidixit829@gmail.com>
2026-06-13 20:09:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import google.generativeai as genai
+import asyncio
 
-# Secrets se key uthao
+# Ye line add karo taaki loop ki dikkat na ho
+try:
+    loop = asyncio.get_event_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
+genai.configure(api_key=st.secrets["AQ.Ab8RN6IkIlV3S10-cDuPEkhsSbYaqlYS9QbsXdjgRSajgPFE7w"])
