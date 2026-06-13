5df467d15aa3eb257ae6b4aae34166e028529d81
Abhidixit-11 <abhidixit829@gmail.com>
2026-06-13 21:09:56 +0530

    Update app.py
    FIXED SECRET KEY

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ import google.generativeai as genai
 import asyncio
 
 # Ye line add karo taaki loop ki dikkat na ho
-try:
-    loop = asyncio.get_event_loop()
-except RuntimeError:
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+
 
 genai.configure(api_key=st.secrets["API_KEY"])
